51c085e1e160fee27afec3dd458ceac24ca34b68
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-10-23 20:30:39 +0800

    Variables are added

---
+++ appleandorange.py
@@ -0,0 +1,7 @@
+print('APPLE IS 20PHP EACH WHILE ORANGE IS 25PHP EACH. ')
+apple_str = input('Enter how many apples do you want to buy: ') 
+apple = int(apple_str)
+orange_str = input('Enter how many oranges do you want to buy: ')
+orange = int(orange_str)
+apple_price = 20
+orange_price = 25
