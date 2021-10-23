c07d40910cd183c6b2af68840961802b483cb458
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-10-23 21:12:25 +0800

    Not completed, Print function is still missing

---
+++ moneyandapple.py
@@ -0,0 +1,7 @@
+total_money = float(input('How much money do you have?: '))
+price_apple = int(input('What is the price for each apple: '))
+
+max_apples= total_money // price_apple
+change= total_money % price_apple
+
+
