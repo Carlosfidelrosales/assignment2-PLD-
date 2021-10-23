0585b4b70e9898c180f6f07570927404110ab2e7
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-10-23 19:41:34 +0800

    First Commit

---
+++ nameageaddress.py
@@ -0,0 +1,6 @@
+name = input("Enter your full name: ")
+age_str = input("Enter your age: ")
+age = int(age_str)
+address= input("Enter your full address: ")
+
+print(f"Hi, my name is {name}. I am {age} years old and I live in {address}.")
