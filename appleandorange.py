81d319a8f3a5a7da57f4bdd2b0f4ccdde2978495
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-10-23 20:32:44 +0800

    Program Completed

--- appleandorange.py
+++ appleandorange.py
@@ -1,7 +1,15 @@
 print('APPLE IS 20PHP EACH WHILE ORANGE IS 25PHP EACH. ')
 apple_str = input('Enter how many apples do you want to buy: ') 
 apple = int(apple_str)
+
 orange_str = input('Enter how many oranges do you want to buy: ')
 orange = int(orange_str)
+
 apple_price = 20
 orange_price = 25
+
+apple_total = (apple * apple_price)
+orange_total = (orange * orange_price)
+
+grand_total= apple_total + orange_total
+print(f'The total amount is {grand_total}.')
