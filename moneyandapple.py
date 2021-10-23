cab6ea7443479bfb3f58c176b17e58e26c8883f8
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-10-23 21:14:27 +0800

    Completed, the print function is added

--- moneyandapple.py
+++ moneyandapple.py
@@ -4,4 +4,4 @@ price_apple = int(input('What is the price for each apple: '))
 max_apples= total_money // price_apple
 change= total_money % price_apple
 
-
+print(f"You can buy {max_apples} apples and your change is{change: .2f} pesos")
